6eaa32efb54b08bc19db3fc506fe6061007fb61c
huykemvip1 <huykemvip@gmail.com>
2023-07-19 20:37:09 +0700

    Revert "update code"
    This reverts commit 8a5ed0365f4e82efe2bab52c1f6150ea58a83dd1.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        showData();
-    }
-    private static void showData(){
-        System.out.println("Show data of Java");
     }
 }
