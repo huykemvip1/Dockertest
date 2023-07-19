8a5ed0365f4e82efe2bab52c1f6150ea58a83dd1
huykemvip1 <huykemvip@gmail.com>
2023-07-19 19:44:39 +0700

    update code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        showData();
+    }
+    private static void showData(){
+        System.out.println("Show data of Java");
     }
 }
